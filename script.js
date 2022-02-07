7067d9931153a2dc76b5d687a2e10d2f220b4707
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-02-07 16:01:29 +0500

    Change styles,sttributes and classes

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 // Selecting, creating and deleting elements
 
-/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -73,5 +72,51 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
-*/
 
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+console.log(message.style.color);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'lightblue');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standart
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('a', 'a', 'a');
+logo.classList.remove('a');
+logo.classList.toggle('a');
+logo.classList.contains('a');
+
+// Don't use
+logo.className = 'jonas';
